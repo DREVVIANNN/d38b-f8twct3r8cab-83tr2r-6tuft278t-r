e3e1927ab758e6558e418428a1a193ba97d93fa1
DREVVIANN <nekotopias3@gmail.com>
2025-03-14 11:36:04 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function display() {
+    document.getElementById('textInfo').style.display='block';
+    setTimeout(() => {
+        document.getElementById('textInfo').style.display='none';
+    }, 2000);
+}
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBtmafoTlFm8EARO3i8kKVPOJjVph3On3M",
+    authDomain: "login-77493.firebaseapp.com",
+    projectId: "login-77493",
+    storageBucket: "login-77493.firebasestorage.app",
+    messagingSenderId: "851224192233",
+    appId: "1:851224192233:web:eb95330e8ec6ae326bfc78"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  document.getElementById('login-google').addEventListener('click', function(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // IdP data available in result.additionalUserInfo.profile.
+      // ...
+      window.location.href="https://www.furina.my.id";
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+    alert(errorMessage);
+  });
+  })
